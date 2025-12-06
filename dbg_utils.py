c98a7cba72882bb145502dafb200326394eafaaa
loew-tech <147945762+loew-tech@users.noreply.github.com>
2025-12-06 16:40:09 -0700

    Testing2 (#1)
    * small changes to read_input
    
    * get_test_input function
    
    currently only grabs test input for part 1
    
    * moving get_test_input to dbg_util
    
    * adding testing flag and capability
    
    currently prints output of running on test input but no validation of correctness
    
    * cleaning up comments

--- dbg_utils.py
+++ dbg_utils.py
@@ -1,4 +1,23 @@
-from typing import Iterable
+from typing import Iterable, Tuple, Callable, List
+
+from bs4 import BeautifulSoup
+import requests
+
+
+from constants import ADVENT_URI
+
+
+def get_test_input(
+        day: int | str,
+        year: int | str = 2025,
+) -> str:
+    problem = requests.get(f'{ADVENT_URI}{year}/day/{day}')
+    soup = BeautifulSoup(problem.content, 'html.parser')
+    input_ = soup.find('pre').code
+    # @TODO: handle validating against this
+    # @TODO: handle getting answer for part 2
+    # answer = soup.find_all('code')[:-1]
+    return input_.get_text()
 
 
 def print_grid(grid: Iterable[Iterable], spacer='') -> None:
--- utils.py
+++ utils.py
@@ -1,25 +1,37 @@
 from datetime import datetime
 from http import HTTPStatus
 import requests
-from typing import List, Callable
+from typing import List, Callable, Tuple
+
+from bs4 import BeautifulSoup
 
 from constants import ADVENT_URI, DIRECTIONS
+from dbg_utils import get_test_input
 
 
 def read_input(
         day: int | str,
         year: int | str = 2025,
         delim: str = '\n',
-        parse: Callable[[str], any] = None
+        parse: Callable[[str], any] = None,
+        testing: bool = False
 ) -> List[any] | str:
-    year = year if year is not None else datetime.now().year
+    if testing:
+        return _process_input(get_test_input(day, year), delim, parse)
     with open('.env') as env_:
         session_id = env_.read().strip().split('\n')[0]
     response = requests.get(f'{ADVENT_URI}{year}/day/{day}/input',
                             cookies={'session': session_id})
-    data = None
-    if response.status_code == HTTPStatus.OK:
-        data = response.text.strip().split(delim) if delim else response.text
+    if not response.status_code == HTTPStatus.OK:
+        raise Exception(f'Failed to acquire input from {ADVENT_URI}')
+    return _process_input(response.text, delim, parse)
+
+
+def _process_input(
+        text, delim: str,
+        parse: Callable[[List[str] | str], any]
+) -> any:
+    data = text.strip().split(delim) if delim else text
     return data if parse is None else [
         parse(e) for e in (data if type(data) == list else [data])
     ]
@@ -35,6 +47,7 @@ def inbounds(y, x: int, grid: List[List[any] | str]) -> bool:
     return 0 <= y < len(grid) and 0 <= x < len(grid[y])
 
 
+# @TODO: move to helpers
 def day_4b_remove_rolls(counts: List[List[int]]):
     to_move = set((y, x) for y, row in enumerate(counts)
                   for x, v in enumerate(row) if v < 4)
