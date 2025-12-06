3c23409fc3ea96b77f34379e9ac0f1afe8e8640e
loew-tech <loew.technology@gmail.com>
2025-12-06 09:03:55 -0700

    day 6a constants

--- constants.py
+++ constants.py
@@ -1,3 +1,6 @@
+from operator import add, mul, sub, truediv, floordiv
+
+
 ADVENT_URI = 'https://adventofcode.com/'
 
 
@@ -7,3 +10,5 @@ DIRECTIONS = tuple((i, j) for i in range(-1, 2)
 
 CARDINAL_DIRECTIONS = tuple((i, j) for i, j in DIRECTIONS
                             if not abs(i) == abs(j))
+
+OPS_DICT = {'+': add, '*': mul, '-': sub, '/': truediv, '//': floordiv}
