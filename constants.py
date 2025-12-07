369793981677c2159a0878ed1a517247962e18a7
loew-tech <loew.technology@gmail.com>
2025-12-06 17:28:24 -0700

    store off local copy of inputs/tests

--- constants.py
+++ constants.py
@@ -3,6 +3,10 @@ from operator import add, mul, sub, truediv, floordiv
 
 ADVENT_URI = 'https://adventofcode.com/'
 
+INPUTS_PATH = 'inputs/'
+
+TESTS_PATH = 'tests/'
+
 
 DIRECTIONS = tuple((i, j) for i in range(-1, 2)
                    for j in range(-1, 2) if not i == j == 0)
--- dbg_utils.py
+++ dbg_utils.py
@@ -1,23 +1,34 @@
-from typing import Iterable, Tuple, Callable, List
+import os
+from typing import Iterable
 
 from bs4 import BeautifulSoup
 import requests
 
 
-from constants import ADVENT_URI
+from constants import ADVENT_URI, TESTS_PATH
 
 
 def get_test_input(
         day: int | str,
         year: int | str = 2025,
 ) -> str:
+    if os.path.exists(f'{TESTS_PATH}{day}.txt'):
+        print(f'retrieving {day} test from file')
+        with open(f'{TESTS_PATH}{day}.txt') as in_:
+            return in_.read()
+
     problem = requests.get(f'{ADVENT_URI}{year}/day/{day}')
     soup = BeautifulSoup(problem.content, 'html.parser')
     input_ = soup.find('pre').code
     # @TODO: handle validating against this
     # @TODO: handle getting answer for part 2
     # answer = soup.find_all('code')[:-1]
-    return input_.get_text()
+    txt = input_.get_text()
+    if not os.path.exists(TESTS_PATH):
+        os.mkdir(TESTS_PATH)
+    with open(f'{TESTS_PATH}{day}.txt', 'w') as out:
+        out.write(txt)
+    return txt
 
 
 def print_grid(grid: Iterable[Iterable], spacer='') -> None:
